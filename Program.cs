1555b12f11bb6b40f381a17941d09cb99085dd9b
Benedikt Conrad <ConradBe@Admins-MBP.root.local>
2020-08-19 11:46:32 +0200

    Init Repository

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test_Neu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "<name>", "Ana", "Felipe" };
+            foreach (string name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine($"Hello {names[i].ToUpper()}!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
