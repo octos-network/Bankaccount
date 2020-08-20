d710156ff150e4ca77971fb60abd460ddc8e624a
Benedikt Conrad <ConradBe@Admins-MBP.root.local>
2020-08-20 14:32:36 +0200

    Transaktion jetzt moeglich

--- TestNeu2/Program.cs
+++ TestNeu2/Program.cs
@@ -6,7 +6,10 @@ namespace TestNeu2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = new BankAccount("Dallas", 100000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            Console.ReadKey();
         }
     }
 }
